33958dc7d15dea1fe3aeb3f9f7934a0a4566c760
jwimpeney <56587173+jwimpeney@users.noreply.github.com>
2019-10-15 14:35:47 +0100

    I'm testing to see how committing from VS works

--- Jon's cool lad folder/Python scripts/TimeforPython/TimeforPython/TimeforPython.py
+++ Jon's cool lad folder/Python scripts/TimeforPython/TimeforPython/TimeforPython.py
@@ -1,1 +1,11 @@
-name = input("Please enter your name.")
+
+validNameEntered=False
+while(validNameEntered == False):
+    name = input("Please enter your name. \n")
+    if input:
+        print(name + "Is really gay gotteeem")
+    else:
+        print("Please enter a valid name")
+
+if (true):
+    print ("dab")
