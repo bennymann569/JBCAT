41287d76a2274109d109079e34e305b3d19dc021
jwimpeney <56587173+jwimpeney@users.noreply.github.com>
2019-10-15 14:54:53 +0100

    Made a really shit python script

--- Jon's cool lad folder/Python scripts/TimeforPython/TimeforPython/TimeforPython.py
+++ Jon's cool lad folder/Python scripts/TimeforPython/TimeforPython/TimeforPython.py
@@ -1,11 +1,11 @@
-
 validNameEntered=False
 while(validNameEntered == False):
     name = input("Please enter your name. \n")
     if input:
         print(name + "Is really gay gotteeem")
+        validNameEntered = True
     else:
         print("Please enter a valid name")
-
-if (true):
+input("Press any key and watch the world descend to chaos :D")
+while (True):
     print ("dab")
